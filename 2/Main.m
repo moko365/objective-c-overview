a32a616877afaae9afc126e3a90d00a95f4951e7
jollen <jollen@moko365.com>
2013-08-30 23:23:01 +0800

    first commit

---
+++ 2/Main.m
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char *argv[])
+{
+   @autoreleasepool {
+   	  int	x;
+   	  float	y;
+
+   	  x = 10;
+   	  y = 3.14;
+
+      NSLog(@"Programming is fun!");
+      NSLog(@"x = %d, y = %f", x, y);
+   }
+   return 0;
+}
---
+++ 3/Main.m
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char *argv[])
+{
+   @autoreleasepool {
+   		float weight;
+
+   		weight = 58.5;
+
+   		NSLog(@"Weight = %f kg\n", weight);
+   }
+   return 0;
+}
